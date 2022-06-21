150030d57034b70c92b0171fc93dbb3227b7922d
Thomas Howard <thomashoward206@gmail.com>
2022-06-21 15:02:52 -0700

    1.5

--- Selenium/espn_Homepage.py
+++ Selenium/espn_Homepage.py
@@ -3,10 +3,12 @@ from selenium import webdriver
 driver = webdriver.Chrome(executable_path="/Users/thomashoward/Desktop/chromedriver")  # driver object
 
 driver.get("https://www.espn.com")  #get method used for envoking the browser
+driver.maximize_window()
 
 print(driver.title)
 print(driver.current_url)
 
-
+driver.get("https://www.espn.com/mlb/")
+driver.back()  #back to homepage of ESPN
 
 driver.close()
