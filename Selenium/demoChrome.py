01726119996750b36e01cf3fde54d809da47e903
Thomas Howard <thomashoward206@gmail.com>
2022-06-21 13:57:47 -0700

    1.5

--- Selenium/demoChrome.py
+++ Selenium/demoChrome.py
@@ -3,4 +3,10 @@ from selenium import webdriver
 driver = webdriver.Chrome(executable_path="/Users/thomashoward/Desktop/chromedriver")  # driver object
 
 driver.get("https://www.espn.com")  #get method used for envoking the browser
+
+print(driver.title)
+print(driver.current_url)
+
+
+
 driver.close()
