96ce0c9933c849518b5632fd7dbc0d8a5721e7ec
Thomas Howard <thomashoward206@gmail.com>
2022-06-21 12:54:55 -0700

    1.5

--- Basics/trycatch.py
+++ Basics/trycatch.py
@@ -1,8 +1,8 @@
 
 # try , catch
 try:
-    with open("filelog.txt", 'r') as reader:
+    with open("test.txt", 'r') as reader: #test.txt will pass any other filename will not
         reader.read()
 
 except:
-    print("this is a block--failed test")
+    print("this is a customized error message if the test fails")
