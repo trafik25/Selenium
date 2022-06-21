83df51abd7568b5383134c31a572d610e9931308
Thomas Howard <thomashoward206@gmail.com>
2022-06-21 09:26:09 -0700

    1.1

--- Basics/writeafile.py
+++ Basics/writeafile.py
@@ -6,4 +6,7 @@
 
 with open('test.txt', 'r') as reader:
     content = reader.readlines()
-
+    reversed(content)
+    with open('test.txt', 'w') as writer:
+        for line in reversed(content):
+            writer.write(line)
